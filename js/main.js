ea96df7fc65554689803e3ccc39adbc9a55d2797
Kyle <the.kyle.billings@gmail.com>
2016-09-13 10:20:19 -0700

    cool

---
+++ js/main.js
@@ -0,0 +1,61 @@
+function resizenow() {
+    var browserwidth = jQuery(window).width();
+    var browserheight = jQuery(window).height();
+    jQuery('.bonfire-pageloader-icon').css('right', ((browserwidth - jQuery(".bonfire-pageloader-icon").width())/2)).css('top', ((browserheight - jQuery(".bonfire-pageloader-icon").height())/2));
+};
+resizenow();
+
+
+jQuery(window).resize(function(){
+     resizenow();
+});
+function resizenow() {
+    var browserwidth = jQuery(window).width();
+    var browserheight = jQuery(window).height();
+    jQuery('.bonfire-pageloader-icon').css('right', ((browserwidth - jQuery(".bonfire-pageloader-icon").width())/2)).css('top', ((browserheight - jQuery(".bonfire-pageloader-icon").height())/2));
+};
+resizenow();
+
+jQuery("html").addClass('bonfire-html-onload');
+
+jQuery(document.body).on("touchmove", function(e) {
+    e.preventDefault();
+});
+
+
+var scrollPosition = [
+self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
+self.pageYOffset || document.documentElement.scrollTop  || document.body.scrollTop
+];
+var html = jQuery('html');
+html.data('scroll-position', scrollPosition);
+html.data('previous-overflow', html.css('overflow'));
+html.css('overflow', 'hidden');
+window.scrollTo(scrollPosition[0], scrollPosition[1]);
+
+jQuery(window).load(function() {
+
+    setTimeout(function(){
+    jQuery(".bonfire-pageloader-icon").addClass('bonfire-pageloader-icon-hide');
+    },1500);
+
+    setTimeout(function(){
+        jQuery(document.body).unbind('touchmove');
+
+        var html = jQuery('html');
+        var scrollPosition = html.data('scroll-position');
+        html.css('overflow', html.data('previous-overflow'));
+        window.scrollTo(scrollPosition[0], scrollPosition[1]);
+
+        jQuery("#bonfire-pageloader").addClass('bonfire-pageloader-fade');
+
+        jQuery("html").removeClass('bonfire-html-onload');
+    },1750);
+
+    setTimeout(function(){
+
+        jQuery("#bonfire-pageloader").addClass('bonfire-pageloader-hide');
+
+    },3500);
+
+});
